c0a28eea6f7776f3cb7a03fbd66c2ade5341163e
ukraine\OYesipov <oleksandr.yesipov@avanquest.com>
2022-07-02 11:38:56 +0300

    add helperGetDateChat

---
+++ TelegramChatBlazor.Web/Helper/Datetime.cs
@@ -0,0 +1,28 @@
+﻿namespace TelegramChatBlazor.Web.Helper
+{
+    public static class Datetime
+    {
+        public static DateTime? tempDate = null;
+        public static string GetDateChat(DateTime dateTime)
+        {
+            if (tempDate?.Date == dateTime.Date)
+            {
+                return null;
+            }
+
+            tempDate = dateTime;
+
+            if (dateTime.Date.Date == DateTime.Now.Date)
+            {
+                return "Today";
+            }
+
+            if (dateTime.Date.Date == DateTime.Now.AddDays(-1).Date)
+            {
+                return "Yesterday";
+            }
+
+            return dateTime.ToString($"MMM d") + $"th, {dateTime.Year}";
+        }
+    }
+}
