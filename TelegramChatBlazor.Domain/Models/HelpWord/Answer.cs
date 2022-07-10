7b105775e6e0183ff60f4305ea4cd46621760c83
ukraine\OYesipov <oleksandr.yesipov@avanquest.com>
2022-07-10 19:10:38 +0300

    add pages for CRUD answer

--- TelegramChatBlazor.Domain/Models/HelpWord/Answer.cs
+++ TelegramChatBlazor.Domain/Models/HelpWord/Answer.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TelegramChatBlazor.Domain.Models.HelpWord;
+﻿namespace TelegramChatBlazor.Domain.Models.HelpWord;
 
 public class Answer
 {
