58fed93afaa39fc14b4ae4d5443d27edaaf82bef
ukraine\OYesipov <oleksandr.yesipov@avanquest.com>
2022-07-11 18:27:21 +0300

    add autoHeightTextarea

--- TelegramChatBlazor.Web/wwwroot/js/scripts.js
+++ TelegramChatBlazor.Web/wwwroot/js/scripts.js
@@ -1,4 +1,15 @@
 ﻿
+window.autoHeightTextarea = () => {
+    const textarea = document.getElementsByTagName("textarea")[0];
+        textarea.setAttribute("style", "height:" + (textarea.scrollHeight) + "px;overflow-y:hidden;");
+        textarea.addEventListener("input", OnInput, false);
+    
+    function OnInput() {
+        this.style.height = "auto";
+        this.style.height = (this.scrollHeight) + "px";
+    }
+}
+
 window.jsMethod = () => {
     var margin = 10,
         instance3 = new emojiButtonList("emojiButton", {
@@ -9,7 +20,6 @@ window.jsMethod = () => {
             xAlignMargin: margin
         });
 }
-
 
 
 window.scrollToElementId = (elementId) => {
